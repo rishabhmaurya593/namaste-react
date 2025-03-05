6cdb6fe7d6113b7e4ba9db51b76322f5c390d42a
Rishabh Maurya <rishabhmauryaaie@gmail.com>
2025-03-05 17:52:01 +0530

    Episode3 part-4

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag."),
-    React.createElement("h2", {}, "I'm h2 tag."),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag."),
-    React.createElement("h2", {}, "I'm h2 tag."),
-  ]),
-]);
 
+// JSX => HTML like or XML like syntax.
+// JSX => Babel transpiles it to React.createElement => React Element-JS object => HTML Element
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+
+// we need to wrap in parenthesis our JSX if we want to write in multiple lines.
+const jsxHeading = (
+<h1 id="heading" className='head'>
+  Namaste React!
+  </h1>
+)
+
+console.log(jsxHeading)
+root.render(jsxHeading)
+
